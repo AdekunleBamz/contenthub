467792eddf2a379622388d817669bb28e6e68bbe
AdekunleBamz <bams.kunle@gmail.com>
2026-04-23 03:15:32 +0100

    Add utility test coverage

---
+++ tests/utils.test.ts
@@ -0,0 +1,37 @@
+import test from 'node:test'
+import assert from 'node:assert/strict'
+import {
+  buildIpfsUrl,
+  bytesToMb,
+  capitalize,
+  clamp,
+  formatDate,
+  formatFileSize,
+  formatNumber,
+  formatWei,
+  getInitials,
+  ipfsToHttp,
+  isAllowedMimeType,
+  isNonEmptyString,
+  isPositiveInteger,
+  isValidAddress,
+  isValidCid,
+  isValidDescription,
+  isValidFileSize,
+  isValidTags,
+  isValidTitle,
+  isZeroAddress,
+  omit,
+  pick,
+  pluralize,
+  range,
+  safeJsonParse,
+  shortAddress,
+  toSlug,
+  truncate,
+  uniqueBy,
+} from '../lib/utils'
+
+test('shortAddress truncates long addresses', () => {
+  assert.equal(shortAddress('0x1234567890abcdef1234567890abcdef12345678'), '0x1234...5678')
+})
